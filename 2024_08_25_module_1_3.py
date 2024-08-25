e06afeac00972136246e10455d14a0231ef9fd26
lomaster064 <64ilya64@gmail.com>
2024-08-25 12:31:37 +0400

    Add files via upload
    Задача по теме динамическая типизация

---
+++ 2024_08_25_module_1_3.py
@@ -0,0 +1,10 @@
+# Динамическая типизация
+
+name = 'Илья'
+print("Name:", name)
+age = 29
+print("Age:", age)
+age = age + 1
+print("New age:",age)
+is_student = True
+print("Is Student:", is_student)
